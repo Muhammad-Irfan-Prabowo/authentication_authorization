f761a19d7b5865da853c2ac6f85ba5801ecc2234
Muhammad-Irfan-Prabowo <if22.muhammadprabowo@mhs.ubpkarawang.ac.id>
2023-11-18 11:30:08 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>login</h1>
+    <form action="./backend/login.php" method="post">
+        <input type="email" name="email" placeholder="masukkan email anda">
+        <input type="password" name="password" placeholder="masukkan password anda">
+        <input type="submit" value="login" name="submit">
+    </form>
+
+    </form>
+    <form action="register.php" method="post">
+        <input id="register-button" type="submit" value="register" name="register">
+    </form>
+
+</body>
+</html>
